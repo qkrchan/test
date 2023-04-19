6e53f2b615d665d7628eaf16948379a67334cc8f
qkrchan <128149466+qkrchan@users.noreply.github.com>
2023-04-20 07:39:33 +0900

    Add files via upload

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -6,6 +6,9 @@ const colorOption = Array.from(document.getElementsByClassName("color-option"));
 const modeBtn = document.querySelector("#mode-btn");
 const destroyBtn = document.querySelector("#destroy-btn");
 const eraserBtn = document.querySelector("#eraser-btn");
+const fileInput = document.querySelector("#file");
+const textInput = document.querySelector("#text");
+const saveBtn = document.querySelector("#save");
 
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
@@ -13,9 +16,11 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
+
+
 let isPainting = false;
 let isFilling = false;
-
 
 function onMove(event){
   if(isPainting){
@@ -68,8 +73,35 @@ function onEraserClick(){
   isFilling = false;
   modeBtn.innerText = "Fill";
 }
+function onFileChange(event){
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image;
+  image.src = url;
+  image.onload = function(){
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  }
+}
+function onDoubleClick(event){  
+  const text = textInput.value;
+  if(text !== ""){
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "48px serif"
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
+function onSaveClick(){
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
 
-
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -84,72 +116,7 @@ colorOption.forEach(color => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
 
 
-
-/* const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-]
-
-function onClick(event){
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random()* colors.length)];
-  ctx.strokeStyle = color
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
-  
-}
-
-canvas.addEventListener("mousemove", onClick); */
-
-
-
-/* ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.stroke();
-ctx.beginPath();
-
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
-ctx.fill(); */
-
-/* ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke(); */
-
-/* ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
-ctx.fill();
- */
-
-/* ctx.fillRect(210, 200 + 10, 15, 100);
-ctx.fillRect(350, 200 + 10, 15, 100);
-ctx.fillRect(260, 200 + 10, 60, 100);
-
-ctx.arc(290, 150, 50, 0, 2*Math.PI);
-ctx.fill();
-ctx.beginPath();
-
-ctx.arc(270, 150 - 5, 8, Math.PI, 2*Math.PI);
-ctx.arc(310, 150 - 5, 8, Math.PI, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill(); */
